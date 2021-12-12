a613b391786750d29b9abbfb1c8fd38f54dc93c8
nath_brl <70270233+nathbrl@users.noreply.github.com>
2021-12-12 20:29:08 +0100

    product.js
    check if product already exist in cart

--- P5-Dev-Web-Kanap-master/front/javascript/cart.js
+++ P5-Dev-Web-Kanap-master/front/javascript/cart.js
@@ -1,74 +1,16 @@
-/* Page produit */
-/*function savingProductId(sofas) {
-    const addToCartButton = document.querySelectorAll('#addToCart');
-    //console.log(addToCartButton);
-    addToCartButton.forEach((button) => {
-        button.addEventListener('click', () => {
-            const productId = getProductUrl();
-            //console.log(productId);
-            const getProduct = sofas.filter(sofa => sofa._id == productId);
-            //console.log(getProduct);
-            storeProducts(getProduct);
-        });
-    });
-}
-/* page produit
-function storeProducts(sofa) {
-    const productName = sofa[0].name;
-    //console.log(productName);
-    const productPrice = sofa[0].price;
-    //console.log(productPrice);
-    const productImg = sofa[0].imageUrl;
-    //console.log(productImg);
-    const altTxt = sofa[0].altTxt;
-    //console.log(altTxt);
-    const productId = localStorage.getItem('productId');
-    //console.log(productId);
-    const selectedColor = localStorage.getItem('selectedColor');
-    let selectedQuantity = localStorage.getItem('selectedQuantity');
-    //console.log(selectedQuantity);
-    //console.log(selectedColor);
-    let productDescription = {
-        productId: productId,
-        selectedColor: selectedColor,
-        selectedQuantity: parseInt(selectedQuantity),
-        productName: productName,
-        productPrice: productPrice,
-    }
-    //console.log(product);
-    cart = JSON.parse(localStorage.getItem('products')) || [];
-    cart.push(productDescription);
-    cart.forEach((product) => {
-        //console.log(product);
-        if (localStorage.getItem('products') === null) {
-            console.log('cart is null');
-        } else if(localStorage.getItem('selectedColor') === product.selectedColor
-                && localStorage.getItem('productId') === product.productId) {
-                    product.selectedQuantity = parseInt(product.selectedQuantity) + productOptions.selectedQuantity;
-                    console.log(productOptions.selectedQuantity);
-                    console.log(product.selectedQuantity);
-        } else {
-            cart.push(productOptions);
-            localStorage.setItem("products", JSON.stringify(cart));
-        }
-    })
-}*/
-
-function displayCartItems() {
+function displayCartProducts() {
     let divProduct = document.querySelector("#cart__items");
-    //console.log(divProduct);
     let cartItems = localStorage.getItem("products");
     cartItems = JSON.parse(cartItems);
-    //console.log(cartItems);
+    console.log(cartItems);
    
     if (cartItems && divProduct) {
         divProduct.innerHTML = '';
         cartItems.forEach((product) => {
-            //console.log(product);
             divProduct.innerHTML += `
             <article class="cart__item" data-id=${product.productId}>
                 <div class="cart__item__img">
-                    <img src=${product.productImg} alt=${product.imgAltTxt}>
+                    <img src=${product.productImage}>
                 </div>
                 <div class="cart__item__content">
                     <div class="cart__item__content__titlePrice">
@@ -91,6 +33,7 @@ function displayCartItems() {
         });
     }
 }
+displayCartProducts();
 
 /*function checkFormValidity() {
     const firstName = document.querySelector('#firstName');
--- P5-Dev-Web-Kanap-master/front/javascript/product.js
+++ P5-Dev-Web-Kanap-master/front/javascript/product.js
@@ -11,7 +11,7 @@ fetch(`http://localhost:3000/api/products/${productId}`)
     .then((res) => res.json())
     .then((sofa) => {
         displayProductDetails(sofa);
-        saveProductToLocalStorage(sofa);
+        addToCart(sofa);
     });
 
 function displayProductDetails(sofa) {
@@ -41,7 +41,8 @@ function displayProductDetails(sofa) {
 
 let cart = JSON.parse(localStorage.getItem('products')) || [];
   
-function saveProductToLocalStorage(sofa) {
+function addToCart(sofa) {
+    const productImage = sofa.imageUrl;
     const addToCartButton = document.querySelector('#addToCart');
     let select = document.querySelector('#colors');
     let quantity = document.querySelector('input#quantity');
@@ -58,46 +59,23 @@ function saveProductToLocalStorage(sofa) {
     addToCartButton.addEventListener('click', () => {
 
         let productDescription = {
+            productImage: productImage,
             productId: productId,
             selectedColor: selectedColor,
             selectedQuantity: selectedQuantity,
             productName: productName,
             productPrice: productPrice,
         }
-        /*Lors de l'ajout du panier en particulier, voici les étapes à réaliser:
 
-        - au click de mon bouton "Ajouter au panier"
-                    
-        > je vérifie si mon localStorage est vide ou non
-                    
-        >>> Il n'est pas vide : alors je parcours mon localStorage et pour chaque produit qu'il 
-        contient, je vérifie si l'id (identifiant unique) du produit que je veux mettre dans mon 
-        panier est le même que celui du produit que je parcours ET si la couleur (option) 
-        est aussi la même
-
-        >>> Il est vide : alors je rajoute le produit de la page sur laquelle je suis en prenant 
-        bien soin de le mettre dans un tableau (quid de l'ajout d'un autre produit derrière)
-                    
-        si c'est le cas, alors la quantité du produit que je parcours 
-        est égale à elle-même + la quantité du produit que j'ajoute
-                    
-        si ce n'est pas le cas je rajoute le produit de la page sur laquelle 
-        je suis dans le tableau*/
-
-        let emptyLocalStorage = localStorage.getItem('products') === null;
-        console.log(emptyLocalStorage);
-        let sameColor = productDescription.selectedColor === productDescription.selectedColor;
-        console.log(sameColor);
-        let sameId = productDescription.productId === productDescription.productId;
-        console.log(sameId);
-
-        if (emptyLocalStorage) {
-            console.log("je suis dans ma 1è condition localstorage plein");
-            cart.push(productDescription);
+        if (cart.some((item) => item.selectedColor === productDescription.selectedColor) 
+        && cart.some((item) => item.productId === productDescription.productId)) {
+            console.log('product is already in cart');
+            console.log(productDescription.selectedQuantity);
         } else {
-            console.log('je suis dans ma 2è condition localstorage vide');
-            productDescription.selectedQuantity = productDescription.selectedQuantity;
+            console.log('product is not in cart yet');
+            productDescription.selectedQuantity = selectedQuantity;
             cart.push(productDescription);
+            console.log(cart);
         }
         localStorage.setItem('products', JSON.stringify(cart));
     });
