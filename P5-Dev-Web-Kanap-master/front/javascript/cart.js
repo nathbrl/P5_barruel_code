f5a6ba1ccc982426384654fb864c553280d596d5
nathbrl <70270233+nathbrl@users.noreply.github.com>
2022-01-17 18:00:32 +0100

    cart page / confirmation page
    Input verification

--- P5-Dev-Web-Kanap-master/front/javascript/cart.js
+++ P5-Dev-Web-Kanap-master/front/javascript/cart.js
@@ -1,3 +1,5 @@
+
+
 let divProduct = document.querySelector("#cart__items");
 let cartItems = JSON.parse(localStorage.getItem('products'));
 
@@ -32,26 +34,29 @@ if (cartItems && divProduct) {
 
 const totalArticlesQuantity = document.querySelector('#totalQuantity');
 let inputQuantity = document.querySelectorAll('#quantity');
-inputQuantity.forEach((item) => {
-    console.log(item.value);
-    console.log(item.value * item.productSelectedQuantity);
-    totalArticlesQuantity.innerHTML = item.value + ' ';
-})
-
-
-
-let totalArticles = document.querySelector('#totalPrice');
 cartItems.forEach((item, i) => {
-    console.log(total = item.productPrice * item.productSelectedQuantity);
-    totalArticles.innerHTML = total*[i];
+    let totalArticlesInCart = item.productSelectedQuantity;
+    for(let i = 0; i < cartItems[i].length; i++) {
+        totalArticlesInCart = item.productSelectedQuantity;
+    }
+    //console.log(totalArticlesInCart);
+    totalArticlesQuantity.innerHTML = totalArticlesInCart + ' ';
 })
+
+/*let totalArticles = document.querySelector('#totalPrice');
+cartItems.forEach((item, i) => {
+    console.log(total = cartItems[i].productSelectedQuantity * item.productPrice);
+    console.log(sum(item.productPrice));
+    totalArticles.innerHTML = total;
+})*/
+
 
 const suppressButtons = document.querySelectorAll('.deleteItem');
 
 suppressButtons.forEach((button, i) => {
     if (cartItems.filter(item => item.productSelectedColor === cartItems[i].productSelectedColor 
         && item.currentProductId === cartItems[i].currentProductId)) {
-            console.log("ok");
+            //console.log("ok");
             
         button.addEventListener('click', () => {
             console.log("button event ok");
@@ -63,22 +68,87 @@ suppressButtons.forEach((button, i) => {
     }
 })
 
-
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const address = document.querySelector("#address");
 const city = document.querySelector("#city");
 const email = document.querySelector("#email");
 const form = document.querySelector(".cart__order__form");
-const inputs = document.querySelectorAll('.cart__order__form input');
-console.log(inputs);
 
-inputs.forEach((input) => {
-    input.addEventListener('change', e => { // input un à un firstName, lastName etc
-        console.log(inputs);
-        console.log(e);
-    })
-});
+function checkFirstName() {
+    firstName.addEventListener('change', e => {
+        if (!firstName.value.match(regexForName)) {
+            firstNameErrorMsg.innerHTML = "le champ prénom contient des erreurs";
+            firstName.style.border = 'solid 2px red';
+            //window.location = 'cart.html';
+        } else if (firstName.value.match(regexForName)) {
+            firstName.style.border = 'solid 2px #D5FCB4';
+        } else {
+            checkFormValidity();
+        }
+    });
+}
+checkFirstName();
+
+function checkLastName() {
+    lastName.addEventListener('change', e => {
+        if (!lastName.value.match(regexForName)) {
+            lastNameErrorMsg.innerText = "le champ nom contient des erreurs";
+            lastName.style.border = 'solid 2px red';
+            //window.location = 'cart.html';
+        } else if (lastName.value.match(regexForName)) {
+            lastName.style.border = 'solid 2px #D5FCB4';
+        } else {
+            checkFormValidity();
+        }
+    });
+}
+checkLastName();
+
+function checkAddress() {
+    address.addEventListener('change', e => {
+        if (!address.value.match(regexForAddress)) {
+            addressErrorMsg.innerHTML = "le champ addresse contient des erreurs";
+            address.style.border = 'solid 2px red';
+            //window.location = 'cart.html';
+        } else if (address.value.match(regexForAddress)) {
+            address.style.border = 'solid 2px #D5FCB4';
+        } else {
+            checkFormValidity();
+        }
+    });
+}
+checkAddress();
+
+function checkCity() {
+    city.addEventListener('change', e => {
+        if (!city.value.match(regexForName)) {
+            cityErrorMsg.innerHTML = "le champ ville contient des erreurs";
+            city.style.border = 'solid 2px red';
+            //window.location = 'cart.html';
+        } else if (city.value.match(regexForName)) {
+            city.style.border = 'solid 2px #D5FCB4';
+        } else {
+            checkFormValidity();
+        }
+    });
+}
+checkCity();
+
+function checkEmail() {
+    email.addEventListener('change', e => {
+        if (!email.value.match(regexForEmail)) {
+            emailErrorMsg.innerHTML = "le champ email contient des erreurs";
+            email.style.border = 'solid 2px red';
+            //window.location = 'cart.html';
+        } else if (email.value.match(regexForEmail)) {
+            email.style.border = 'solid 2px #D5FCB4';
+        } else {
+            checkFormValidity();
+        }
+    });
+}
+checkEmail();
 
 const firstNameErrorMsg = document.getElementById('firstNameErrorMsg');
 const lastNameErrorMsg = document.getElementById('lastNameErrorMsg');
@@ -86,58 +156,64 @@ const addressErrorMsg = document.getElementById('addressErrorMsg');
 const cityErrorMsg = document.getElementById('cityErrorMsg');
 const emailErrorMsg = document.getElementById('emailErrorMsg');
 
-const regexForName = /^([a-zA-Zàâäéèêëïîôöùûüç' ]+)$/;
+const regexForName = /^([a-zA-Zàâäéèêëïîôöùûüç' ]+){3,20}$/;
 const regexForAddress = /^([0-9]{1,3}(([,. ]?){1}[a-zA-Zàâäéèêëïîôöùûüç' ]+))$/;
-const regForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function checkFormValidity() {
     form.addEventListener("submit", (e) => {
-
         e.preventDefault();
-
-        if (!firstName.value.match(regexForName)) {
-            firstNameErrorMsg.innerHTML = "le champ prénom contient des erreurs";
-            firstName.style.border = 'solid 2px red';
-            //window.location = 'cart.html';
-        } else if (firstName.value.match(regexForName)) {
-            firstName.style.border = 'solid 2px #D5FCB4';
-        }
-        if (!lastName.value.match(regexForName)) {
-            lastNameErrorMsg.innerText = "le champ nom contient des erreurs";
-            lastName.style.border = 'solid 2px red';
-            //window.location = 'cart.html';
-        } else if (lastName.value.match(regexForName)) {
-            lastName.style.border = 'solid 2px #D5FCB4';
-        }
-        if (!address.value.match(regexForAddress)) {
-            addressErrorMsg.innerHTML = "le champ addresse contient des erreurs";
-            address.style.border = 'solid 2px red';
-            //window.location = 'cart.html';
-        } else if (address.value.match(regexForAddress)) {
-            address.style.border = 'solid 2px #D5FCB4';
-        }
-        if (!city.value.match(regexForName)) {
-            cityErrorMsg.innerHTML = "le champ ville contient des erreurs";
-            city.style.border = 'solid 2px red';
-            //window.location = 'cart.html';
-        } else if (city.value.match(regexForName)) {
-            city.style.border = 'solid 2px #D5FCB4';
-        }
-        if (!email.value.match(regForEmail)) {
-            emailErrorMsg.innerHTML = "le champ email contient des erreurs";
-            email.style.border = 'solid 2px red';
-            //window.location = 'cart.html';
-        } else if (email.value.match(regForEmail)) {
-            email.style.border = 'solid 2px #D5FCB4';
-        } else {
-            inputs.style.border = 'solid 2px #D5FCB4';
-        //sendOrder();
-        
+        if (!email.value.match(regexForEmail) ||
+            !city.value.match(regexForName) ||
+            !address.value.match(regexForAddress) ||
+            !lastName.value.match(regexForName) ||
+            !firstName.value.match(regexForName)) {
+            console.log('le form ne sera pas envoyé');
+        } else{
+            sendOrder();
+            console.log('le form a été envoyé');
         }
     });
 }
-
 checkFormValidity();
-/*function sendOrder() {
 
-}*/
+const firstNameValue = document.querySelector("#firstName").value;
+const lastNameValue = document.querySelector("#lastName").value;
+const addressValue = document.querySelector("#address").value;
+const cityValue = document.querySelector("#city").value;
+const emailValue = document.querySelector("#email").value;
+
+const products = Object.values(cartItems).map((product) => {
+     //console.log(product);
+    return product;
+});
+
+function sendOrder() {
+    const order = {
+        contact: {
+            firstName: firstNameValue,
+            lastName: lastNameValue,
+            address: addressValue,
+            city: cityValue,
+            email: emailValue,
+        },
+        products: {
+            products: products,
+        }
+    }
+    const body = JSON.stringify(order);
+    
+    fetch('http://localhost:3000/api/products/order', {
+        method: "POST",
+        body: body,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    .then((res) => res.json())
+    .then (res => {
+        const orderI = res.orderId;
+        console.log(orderI);
+        window.location = `confirmation.html?orderId=${orderI}`
+    })
+}
--- P5-Dev-Web-Kanap-master/front/javascript/confirmation.js
+++ P5-Dev-Web-Kanap-master/front/javascript/confirmation.js
@@ -1,7 +1,19 @@
-function getOrderData() {
-    
+console.log('ok');
+
+function getOrderId() {
+    const urlLoc = window.location.href;
+    console.log(urlLoc);
+    const url = new URL(urlLoc);
+    console.log(url);
+    const urlId = url.searchParams.get("id");
+    console.log(urlId);
+    return urlId;
 }
-getOrderData();
+getOrderId();
+
+const orderId = document.getElementById('#orderId');
+console.log(orderId);
+//orderId.innerHTML = getOrderId();
 
 //Clearing LocalStorage items 
-localStorage.clear();
+//localStorage.clear();
